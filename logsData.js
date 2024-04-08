3693369e6119d5dfc47f24daa9e2ff30c4fb72f4
Yuki Shiroi <134266235+casperkids@users.noreply.github.com>
2024-04-08 15:55:34 +0200

    update logData

---
+++ logsData.js
@@ -0,0 +1,299 @@
+export const journals = [
+    {
+      id: '1711133533',
+      title: 'Protest-Performance',
+      date: '2024-03-22',
+      note: 'Taxifahrer wurden am Freitagabend zu Schauspielern: Sie protestierten gegen die Umbenennung des Mercedes-Platzes und der beiden Veranstaltungsorte in Berlin-Friedrichshain.',
+      roboIconSrc: 'https://robohash.org/Protest-Performance'
+    }, 
+      {
+      id: '1711810267',
+      title: 'Read Me',
+      date: '2024-03-20',
+      note: 'arning: branch.main.remote has multiple values',
+      roboIconSrc: 'https://robohash.org/readme'
+    }, 
+    {
+      id: '1710799806',
+      title: 'enough points',
+      date: '2024-03-19',
+      note: 'Miles at smart no marry whole linen mr.',
+      roboIconSrc: 'https://robohash.org/enoughpoints'
+    }, 
+    {
+      id: '1710756606',
+      title: 'sevene',
+      date: '2024-03-19',
+      note: 'Warrant private blushes removed an in equally totally if. ',
+      roboIconSrc: 'https://robohash.org/seven'
+    }, 
+    {
+      id: '1710742206',
+      title: 'gate',
+      date: '2024-03-19',
+      note: ' Alteration connection to so as collecting me.',
+      roboIconSrc: 'https://robohash.org/gate'
+    },
+        {
+      id: '1710885133',
+      title: 'Giorgia',
+      date: '2024-03-19',
+      note: 'Sohn beschuldigt albanische Drogenkuriere des Mordes an seiner Mutter',
+      roboIconSrc: 'https://robohash.org/Giorgia '
+    }, 
+    {
+      id: '1710738616',
+      title: 'material',
+      date: '2024-03-19',
+      note: 'Leaf by left deal mile oh if easy.',
+      roboIconSrc: 'https://robohash.org/material'
+    }, 
+    {
+      id: '1710738606',
+      title: 'offending',
+      date: '2024-03-18',
+      note: 'Estate was tended ten boy nearer seemed.',
+      roboIconSrc: 'https://robohash.org/offending'
+    }, 
+    {
+      id: '1710735006',
+      title: 'breakfast',
+      date: '2024-03-18',
+      note: 'So at parties he warrant oh staying. Square new horses and put better end. ',
+      roboIconSrc: 'https://robohash.org/breakfast'
+    },  
+    {
+      id: '1710130206',
+      title: 'outweigh',
+      date: '2024-03-15',
+      note: 'Added woman first get led joy not early jokes.',
+      roboIconSrc: 'https://robohash.org/outweigh'
+    },  
+          {
+      id: '1710115806',
+      title: 'Small',
+      date: '2024-03-15',
+      note: 'ant four we face an he gate.',
+      roboIconSrc: 'https://robohash.org/small'
+    },  
+        {
+      id: '1709856606',
+      title: 'Now months esteem',
+      date: '2024-03-08',
+      note: 'Outlived it received he material. Him yourself joy moderate off repeated laughter outweigh screened. ',
+      roboIconSrc: 'https://robohash.org/nowmonthsesteem'
+    },  
+      {
+      id: '1709770206',
+      title: 'Sportsman ',
+      date: '2024-03-07',
+      note: 'Income joy nor can wisdom summer. ',
+      roboIconSrc: 'https://robohash.org/sportman'
+    },  
+  {
+    id: '1709683806',
+    title: '30 coolest streets in the world',
+    date: '2024-03-06',
+    note: 'Berlins Oranienstraße, which stretches from Mitte into the heart of Kreuzberg, has been named one of the coolest streets in the world by TimeOut.',
+    roboIconSrc: 'https://robohash.org/30 coolest streets in the world'
+    },
+    {
+    id: ' 1709716206',
+    title: 'plants',
+    date: '2024-03-06',
+    note: 'Last night, I spent a peaceful evening tending to my plants. Each leafy friend received a gentle watering, and I admired their vibrant colors under the soft glow of moonlight.',
+    roboIconSrc: 'https://robohash.org/plants'
+    },
+    {
+    id: '1709698206',
+    title: 'Movie Night',
+    date: '2024-03-06',
+    note: 'Last night, I hosted an epic movie night with friends, and it was a blast! We kicked off the evening with a classic 80s flick, "Back to the Future. The nostalgia hit hard as we followed Marty McFlys time-traveling adventures. Next up was a suspenseful thriller, Inception, which kept us on the edge of our seats until the credits rolled.',
+    roboIconSrc: 'https://robohash.org/movienight'
+    },
+    {
+      id: '1709611806',
+      title: 'Apple cake',
+      date: '2024-03-05',
+      note: 'I indulged in a slice of homemade apple cake, its tender crumb bursting with the comforting flavors of cinnamon and baked apples, evoking memories of crisp autumn days and warm gatherings.',
+      roboIconSrc: 'https://robohash.org/applecake'
+    },
+    {
+      id: '1709525406',
+      title: 'Streik Berlin',
+      date: '2024-03-04',
+      note: 'The Berlin strike prevented me from returning from Düsseldorf, but I got to stay in a luxury hotel for free Lucky or unlucky?',
+      roboIconSrc: 'https://robohash.org/streikberlin'
+    },
+    {
+      id: '1709376733',
+      title: 'Happy Birthday!!',
+      date: '2024-03-04',
+      note: 'So heres to another year of laughter, love, and endless possibilities. Heres to embracing every moment, cherishing every memory, and living life to the fullest.',
+      roboIconSrc: 'https://robohash.org/happybirthday'
+    },
+    {
+      id: '1709405533',
+      title: 'Extra Fresh!!',
+      date: '2024-03-02',
+      note: 'Fruit two match men you seven share. ',
+      roboIconSrc: 'https://robohash.org/extrafresh'
+    },
+    {
+      id: '1711602606',
+      title: 'Sincerity',
+      date: '2024-03-01',
+      note: 'Affronting discretion as do is announcing.',
+      roboIconSrc: 'https://robohash.org/Sincerity'
+    },
+        {
+      id: '1707973806',
+      title: 'direct two men',
+      date: '2024-02-15',
+      note: 'Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. ',
+      roboIconSrc: 'https://robohash.org/directtwomen'
+    },
+    {
+      id: '1707855006',
+      title: 'calender2',
+      date: '2024-02-13',
+      note: 'sed quia non numquam eius modi tempora incidunt ut labore',
+      roboIconSrc: 'https://robohash.org/calender2'
+    },
+    {
+      id: '1707862206',
+      title: 'calender',
+      date: '2024-02-13',
+      note: 'Excepteur sint occaecat cupidatat non proident',
+      roboIconSrc: 'https://robohash.org/calender'
+    },
+        {
+      id: '1706986333',
+      title: 'Demo in Berlin:',
+      date: '2024-02-03',
+      note: 'protestierten am Bundestag gegen Rechtsextremismus',
+      roboIconSrc: 'https://robohash.org/Protest-Performance'
+    }, 
+    {
+      id: '1706764206',
+      title: 'Satisfied',
+      date: '2024-02-01',
+      note: 'Satisfied conveying an dependent contented',
+      roboIconSrc: 'https://robohash.org/satisfied'
+    },
+    {
+      id: '1710532415',
+      title: 'sweetness',
+      date: '2024-01-15',
+      note: 'Ten the hastened steepest feelings pleasant few surprise property. An brother he do colonel against minutes uncivil. Can how elinor warmly mrs basket marked.  ',
+      roboIconSrc: 'https://robohash.org/sweetness'
+    },
+                {
+      id: '1706397644',
+      title: 'Aus dem Nichts',
+      date: '2024-01-27',
+      note: 'Fischers Tränen zum Abschied',
+      roboIconSrc: 'https://robohash.org/Aus dem Nichts'
+    },
+            {
+      id: '1706390444',
+      title: '10 Fragen',
+      date: '2024-01-27',
+      note: 'Schaffst du 90 Prozent, ist dein Allgemeinwissen exzellent',
+      roboIconSrc: 'https://robohash.org/10 Fragen'
+    },
+        {
+      id: '1706379644',
+      title: 'Straßenkampf',
+      date: '2024-01-27',
+      note: 'Straßenkampf der Künste in Manhattan',
+      roboIconSrc: 'https://robohash.org/Straßenkampf'
+    },
+    {
+      id: '1706343644',
+      title: 'Spiel unterbrochen ',
+      date: '2024-01-27',
+      note: 'Ein harter Schuss trifft den Schiedsrichter-Assistenten in Wolfsburg so unglücklich',
+      roboIconSrc: 'https://robohash.org/Spiel unterbrochen'
+    },
+        {
+      id: '1706332844',
+      title: 'Bayern freut sich',
+      date: '2024-01-27',
+      note: 'Der Vorsprung von Bayer Leverkusen auf den FC Bayern beträgt nur noch zwei Punkte. ',
+      roboIconSrc: 'https://robohash.org/Bayern freut sich'
+    },
+    {
+      id: '1705313161',
+      title: 'Extra log',
+      date: '2024-01-15',
+      note: 'An idea is a powerful spark of creativity that fuels innovation, ignites change, and shapes the course of human progress.',
+      roboIconSrc: 'https://robohash.org/extralog'
+    },
+    {
+      id: '1705295161',
+      title: 'Second log',
+      date: '2024-01-15',
+      note: 'Mobile devices have revolutionized communication, enabling instant access to information, services, and entertainment anytime, anywhere, profoundly impacting how we live, work, and interact with the world around us.',
+      roboIconSrc: 'https://robohash.org/secondlog'
+    },
+    {
+      id: '1711601879',
+      title: 'First log',
+      date: '2024-01-11',
+      note: 'Journal log simply records my footsteps.',
+      roboIconSrc: 'https://robohash.org/firstlog'
+    },
+    {
+      id: '1703801406',
+      title: 'Lorem Ipsum',
+      date: '2024-01-10',
+      note: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium',
+      roboIconSrc: 'https://robohash.org/Lorem Ipsum'
+    },
+    {
+      id: '1703715006',
+      title: 'Balck Beans',
+      date: '2024-01-09',
+      note: 'Excepteur sint occaecat cupidatat non proident',
+      roboIconSrc: 'https://robohash.org/Balck Beans'
+    },
+    {
+      id: '1704306828',
+      title: 'Integer tincidunt',
+      date: '2024-01-03',
+      note: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+      roboIconSrc: 'https://robohash.org/Integer tincidunt'
+    },
+    {
+      id: '1704220428',
+      title: 'Aenean imperdiet',
+      date: '2024-01-02',
+      note: 'Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. ',
+      roboIconSrc: 'https://robohash.org/Aenean imperdiet'
+    },
+   
+    {
+      id: '1704202428',
+      title: 'Nam quam',
+      date: '2024-01-02',
+      note: 'Duis leo. Sed fringilla mauris sit ',
+      roboIconSrc: 'https://robohash.org/Nam quam'
+    },
+    {
+      id: '1704116028',
+      title: 'happy new 2024 year',
+      date: '2024-01-01',
+      note: 'ultricies nec, pellentesque eu, pretium quis, sem.',
+      roboIconSrc: 'https://robohash.org/happy new 2024 year'
+    },
+        {
+      id: ' 1704076428',
+      title: 'NEW',
+      date: '2024-01-01',
+      note: 'sed quia consequuntur magni dolores eos qui ratione',
+      roboIconSrc: 'https://robohash.org/new'
+    }
+
+
+  ]
